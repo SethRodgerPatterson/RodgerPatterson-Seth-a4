888403bf750c4bc6495bdd85b3003f741cea3446
SethRodgerPatterson <000915695@mohawkcollege.ca>
2023-12-13 18:58:05 -0500

    Bullets Destroy Collision

--- RodgerPatterson-Seth-a5/Assets/Scripts/Bullet.cs
+++ RodgerPatterson-Seth-a5/Assets/Scripts/Bullet.cs
@@ -24,5 +24,10 @@ public class Bullet : MonoBehaviour
         Destroy(this.gameObject, this.lifeTime);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
 
 }
