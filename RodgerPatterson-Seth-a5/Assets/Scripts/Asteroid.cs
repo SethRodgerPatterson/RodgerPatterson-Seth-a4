ee1bb26a2a4b833346ad81f391f468d1a6fc338f
SethRodgerPatterson <000915695@mohawkcollege.ca>
2023-12-14 03:08:17 -0500

    Prefbae and Uniqueness

---
+++ RodgerPatterson-Seth-a5/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRen;
+    private Rigidbody2D RigidBody;
+
+    public float size = 1.0f;
+    public float minSize = 0.5f;
+    public float maxSize = 1.0f;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Start()
+    {
+        spriteRen.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        this.transform.eulerAngles = new Vector3(0.0f,0.0f,Random.value * 360.0f);
+        this.transform.localScale = Vector3.one * this.size;
+
+        RigidBody.mass = this.size;
+    }
+}
